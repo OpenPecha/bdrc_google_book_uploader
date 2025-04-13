00d76324e7295032648cd442dea0d469f85538f3
Elie Roux <roux.elie@gmail.com>
2025-04-13 16:41:20 +0200

    finish first working example

--- import.py
+++ import.py
@@ -7,11 +7,15 @@ from openpecha.core.pecha import OpenPechaGitRepo, OpenPechaFS
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from openpecha.formatters.ocr import OCRFormatter
 from openpecha.formatters.ocr.hocr import HOCRFormatter, BDRCGBFileProvider
+from openpecha.core.ids import get_initial_pecha_id
 from openpecha.buda.api import get_buda_scan_info
 import boto3
 import os
 from pathlib import Path
+import rdflib
+import requests
 import json
+import shutil
 
 import hashlib
 
@@ -21,10 +25,6 @@ def get_s3_prefix(work_id, batch_num):
     folder_prefix = f'Works/{two}/{work_id}/google_books/{batch_num}/'
     return folder_prefix
 
-TOKEN = os.getenv("GITHUB_TOKEN")
-GIT_CACHE_FOLDER = Path("./cache/git_cache")
-GIT_PULL = False
-ORG_NAME = "Openpecha-data"
 S3_BUCKET = "ocr.bdrc.io"
 
 def get_ocr_import_info(wlname, batch_num, ocr_info):
@@ -33,7 +33,7 @@ def get_ocr_import_info(wlname, batch_num, ocr_info):
         'software': 'google_books',
         'batch': batch_num,
         'expected_default_language': 'bo',
-        'bdrc_scan_id': work_id,
+        'bdrc_scan_id': wlname,
         'ocr_info': ocr_info
     }
 
@@ -71,8 +71,6 @@ def download_ocr(s3_prefix, ocr_disk_path):
     
     print(f"OCR download complete: {s3_prefix} → {ocr_disk_path}")
 
-
-
 def import_w(wlname, batch_num):
     # get buda info for wlname
     buda_info = get_buda_scan_info(wlname)
@@ -81,21 +79,21 @@ def import_w(wlname, batch_num):
     ocr_disk_path = tmp_dir / "gb"
     ocr_disk_path.mkdir(exist_ok=True)
     s3_prefix = get_s3_prefix(wlname, batch_num)
-    download_ocr(s3_prefix, ocr_disk_path)
+    #download_ocr(s3_prefix, ocr_disk_path)
     ocr_info = None
     with open(str(ocr_disk_path / 'info.json')) as f:
         ocr_info = json.load(f)
     ocr_import_info = get_ocr_import_info(wlname, batch_num, ocr_info)
     opf_disk_path = tmp_dir / "opf"
-    #opf_disk_path.mkdir(exist_ok=True)
-    #file_provider = BDRCGBFileProvider(wlname, buda_info, ocr_import_info, ocr_disk_path=ocr_disk_path)
-    #formatter = HOCRFormatter(output_path=opf_disk_path)
-    #pecha_id = get_initial_pecha_id()
-    #opf = formatter.create_opf(file_provider, pecha_id, ocr_import_info=ocr_import_info)
+    opf_disk_path.mkdir(exist_ok=True)
+    file_provider = BDRCGBFileProvider(wlname, buda_info, ocr_import_info, ocr_disk_path=ocr_disk_path)
+    formatter = HOCRFormatter(output_path=opf_disk_path)
+    pecha_id = get_initial_pecha_id()
+    print(f"{wlname},{pecha_id}")
+    opf = formatter.create_opf(file_provider, pecha_id, ocr_import_info=ocr_import_info)
     #opf_repo = OpenPechaGitRepo(pecha_id, opf.opf_path)
     #opf_repo.publish()
-
-
+    shutil.rmtree(str(tmp_dir))
 
 if __name__ == "__main__":
     import_w("W00EGS1016620", "batch_2025")
